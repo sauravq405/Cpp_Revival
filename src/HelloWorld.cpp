0ac7965ef520c072383526195fa9b88dd64c53b1
Saurav Sengupta <saurav.sengupta@oracle.com>
2026-01-13 15:54:59 +0530

    Formatted HelloWorld.cpp

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -2,21 +2,22 @@
 
 /**
  * To compile and run this program from your Cpp_Revival root:
- * * 
+ * *
  * 1. Create build folder:  mkdir build 2>nul
  * 2. Compile:             g++ src/HelloWorld.cpp -o build/HelloWorld.exe
  * OR                      g++ -static src/HelloWorld.cpp -o build/HelloWorld.exe (if above command fails)
  * 3. Run:                 build\HelloWorld.exe
  */
 
-int main() {
+int main()
+{
     // Standard Hello World
     std::cout << "Hello, World! Welcome to Cpp_Revival." << std::endl;
 
     // Printing GCC Version using predefined macros
-    std::cout << "Compiler: GCC " 
-              << __GNUC__ << "." 
-              << __GNUC_MINOR__ << "." 
+    std::cout << "Compiler: GCC "
+              << __GNUC__ << "."
+              << __GNUC_MINOR__ << "."
               << __GNUC_PATCHLEVEL__ << std::endl;
 
     return 0;
